027bfd9ef34afe13d9b5e2e8b99bd539631db926
ROHAAN174 <166196227+ROHAAN174@users.noreply.github.com>
2024-06-03 07:37:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -10,25 +10,6 @@ RESET = colorama.Fore.RESET
 RED = colorama.Fore.RED
 MAGENTA=colorama.Fore.MAGENTA
 colorama.init()
-print(rf"{RED}          ______    __     __    _________    _________                                   ___ ___ ___                 ")
-print(rf"{GREEN}         |  ____|  |  |   |  |  |  _____  |  | ________|                                 |   |   |   |            ")
-print(rf"{GREEN}         | |       |  |   |  |  | |_____| |  | |____                                     |___|___|___|               ")
-print(rf"{GREEN}         | |       |  |   |  |  |  _____  |  |  ____|                                    |   |   |   |            ")
-print(rf"{GREEN}         | |____   |  |___|  |  | |_____| |  | |_______                                  |_N_|_I_|_C_|            ")
-print(rf"{GREEN}         |______|  |_________|  |_________|  |_________|                                 |   |   |   |            ")
-print(rf"{RED}                                                                                         |___|___|___|                     ")    
-print(rf"{RED}                            _______    _________    __      ___      ___   ________    ________         ",end='\n')
-print(rf"{GREEN}                           |  _____|  |   ___   |  |  |     \  \    /  /  |  ______|  |  _____ |    ")
-print(rf"{GREEN}                           | |_____   |  |   |  |  |  |      \  \  /  /   | |____     |  ______|    ")
-print(rf"{GREEN}                           |_____  |  |  |   |  |  |  |       \  \/  /    |  ____|    |   \  \        ")
-print(rf"{GREEN}                            _____| |  |  |___|  |  |  |____    \    /     | |______   |  | \  \       ")
-print(rf"{RED}                           |_______|  |_________|  |_______|    \__/      |________|  |__|  \__\      ")
-
-time.sleep(2)
-print("")
-print("")
-print(f"{MAGENTA}Please refer preview window for which side you have scanned and which color should be in centre on each side. ")
-
 state=  {
             'up':['white','white','white','white','white','white','white','white','white',],
             'right':['white','white','white','white','white','white','white','white','white',],
